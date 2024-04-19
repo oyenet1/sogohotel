373202fdb1271f2632962dc084f180c9e87447e2
oyenet1 <bowofadeoyerinde@gmail.com>
2024-04-19 15:15:46 +0100

    null value removed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,47 +16,160 @@
     <link rel="stylesheet" type="text/css"
         href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
 
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.timepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/fancybox.min.css') }}">
 
-
-    <script src="js/aos.js"></script>
-
-    <script src="js/bootstrap-datepicker.js"></script>
-
+    <link rel="stylesheet" href="{{ asset('frontend/fonts/ionicons/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/fonts/fontawesome/css/font-awesome.min.css') }}">
 
     <!-- Theme Style -->
-    <link rel="stylesheet" href="css/style.css">
-    @vite(['resources/css/app.css', 'resources/js/main.js', 'resources/css/bootstrap.min.css', 'resources/css/animate.css', 'resources/css/owl.carousel.min.css', 'resources/css/aos.css', 'resources/css/bootstrap-datepicker.css', 'resources/css/fancybox.min.css', 'fonts/ionicons/css/ionicons.min.css', 'fonts/fontawesome/css/font-awesome.min.css', 'resources/css/style.css', 'resources/css/jquery.timepicker.css', 'resources/js/app.js', 'resources/js/aos.js', 'resources/js/bootstrap.min.js', 'resources/js/bootstrap-datepicker.js', 'resources/js/main.js', 'resources/js/jquery-3.3.1.min.js', 'resources/js/jquery-migrate-3.0.1.min.js', 'resources/js/jquery.fancybox.min.js', 'resources/js/jquery.magnific-popup.min.js', 'resources/js/jquery.stellar.min.js', 'resources/js/jquery.timepicker.min.js', 'resources/js/jquery.validate.min.js', 'resources/js/jquery.waypoints.min.js', 'resources/js/owl.carousel.min.js', 'resources/js/popper.min.js'])
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+
+    <!-- Theme Style -->
+    {{-- <link rel="stylesheet" href="css/style.css"> --}}
+    {{-- @vite(['resources/js/main.js', 'resources/css/bootstrap.min.css', 'resources/css/animate.css', 'resources/css/owl.carousel.min.css', 'resources/css/aos.css', 'resources/css/bootstrap-datepicker.css', 'resources/css/fancybox.min.css', 'resources/fonts/ionicons/css/ionicons.min.css', 'resources/fonts/fontawesome/css/font-awesome.min.css', 'resources/css/style.css', 'resources/css/jquery.timepicker.css', 'resources/js/app.js', 'resources/js/aos.js', 'resources/js/bootstrap.min.js', 'resources/js/bootstrap-datepicker.js', 'resources/js/main.js', 'resources/js/jquery-3.3.1.min.js', 'resources/js/jquery-migrate-3.0.1.min.js', 'resources/js/jquery.fancybox.min.js', 'resources/js/jquery.magnific-popup.min.js', 'resources/js/jquery.stellar.min.js', 'resources/js/jquery.timepicker.min.js', 'resources/js/jquery.validate.min.js', 'resources/js/jquery.waypoints.min.js', 'resources/js/owl.carousel.min.js', 'resources/js/popper.min.js']) --}}
 
 
     <title>Document</title>
     {{-- @livewireStyles --}}
-    <style>
-        [x-cloak] {
-            display: none;
-        }
-
-        a.hover,
-        a.underline {
-            color: black !important;
-            text-decoration: underline !important;
-        }
-    </style>
     {{-- @livewireStyles --}}
-    @stack('styles')
+    {{-- @stack('styles') --}}
 </head>
 
 {{-- @php
     $permissions = currentUserPermissions();
 @endphp --}}
 
-<body class="bg-background-50">
+<body>
+    <header class="site-header js-site-header">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.html">Sogo Hotel</a></div>
+                <div class="col-6 col-lg-8">
 
+
+                    <div class="site-menu-toggle js-site-menu-toggle" data-aos="fade">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <!-- END menu-toggle -->
+
+                    <div class="site-navbar js-site-navbar">
+                        <nav role="navigation">
+                            <div class="container">
+                                <div class="row full-height align-items-center">
+                                    <div class="col-md-6 mx-auto">
+                                        <ul class="list-unstyled menu">
+                                            <li class="active"><a href="index.html">Home</a></li>
+                                            <li><a href="rooms.html">Rooms</a></li>
+                                            <li><a href="about.html">About</a></li>
+                                            <li><a href="events.html">Events</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="reservation.html">Reservation</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- END head -->
+
+    {{-- main page here --}}
+    @yield('contents')
+
+    <footer class="section footer-section">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col-md-3 mb-5">
+                    <ul class="list-unstyled link">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Terms &amp; Conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Rooms</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 mb-5">
+                    <ul class="list-unstyled link">
+                        <li><a href="#">The Rooms &amp; Suites</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Restaurant</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 mb-5 pr-md-5 contact-info">
+                    <!-- <li>198 West 21th Street, <br> Suite 721 New York NY 10016</li> -->
+                    <p><span class="d-block"><span class="ion-ios-location h5 mr-3 text-primary"></span>Address:</span>
+                        <span> 198
+                            West 21th Street, <br> Suite 721 New York NY 10016</span>
+                    </p>
+                    <p><span class="d-block"><span class="ion-ios-telephone h5 mr-3 text-primary"></span>Phone:</span>
+                        <span> (+1)
+                            435 3533</span>
+                    </p>
+                    <p><span class="d-block"><span class="ion-ios-email h5 mr-3 text-primary"></span>Email:</span>
+                        <span>
+                            info@domain.com</span>
+                    </p>
+                </div>
+                <div class="col-md-3 mb-5">
+                    <p>Sign up for our newsletter</p>
+                    <form action="#" class="footer-newsletter">
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Email...">
+                            <button type="submit" class="btn"><span class="fa fa-paper-plane"></span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row pt-5">
+                <p class="col-md-6 text-left">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> All rights reserved | This template is made with <i class="icon-heart-o"
+                        aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+
+                <p class="col-md-6 text-right social">
+                    <a href="#"><span class="fa fa-tripadvisor"></span></a>
+                    <a href="#"><span class="fa fa-facebook"></span></a>
+                    <a href="#"><span class="fa fa-twitter"></span></a>
+                    <a href="#"><span class="fa fa-linkedin"></span></a>
+                    <a href="#"><span class="fa fa-vimeo"></span></a>
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="{{ asset('frontend/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/popper.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery.fancybox.min.js') }}"></script>
+
+
+    <script src="{{ asset('frontend/js/aos.js') }}"></script>
+
+    <script src="{{ asset('frontend/js/bootstrap-datepicker.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery.timepicker.min.js') }}"></script>
+
+
+
+    <script src="{{ asset('frontend/js/main.js') }}"></script>
 
 
     {{-- @livewireScripts --}}
